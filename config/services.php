a1c366dc42b0f28dcc86d8c403ecd892cf004964
Anton <aner-anton@yandex.ru>
2025-01-16 11:59:37 +0300

    change: ya metrica counter

--- config/services.php
+++ config/services.php
@@ -2,34 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-    ],
-
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
-
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
     'yookassa' => [
         'shop_id' => env('YOOKASSA_SHOP_ID', null),
         'secret_key' => env('YOOKASSA_SECRET_KEY', null)
@@ -39,20 +11,21 @@ return [
         'sms_key' => env('SMS_SERVICE')
     ],
     'vkontakte' => [
-    'client_id' => env('VKONTAKTE_CLIENT_ID'),
-    'client_secret' => env('VKONTAKTE_CLIENT_SECRET'),
-    'redirect' => env('VKONTAKTE_REDIRECT_URI')
+		'client_id' => env('VKONTAKTE_CLIENT_ID'),
+		'client_secret' => env('VKONTAKTE_CLIENT_SECRET'),
+		'redirect' => env('VKONTAKTE_REDIRECT_URI')
     ],
     'odnoklassniki' => [
-    'client_id' => env('ODNOKLASSNIKI_CLIENT_ID'),
-    'client_public' => env('ODNOKLASSNIKI_CLIENT_PUBLIC'),
-    'client_secret' => env('ODNOKLASSNIKI_CLIENT_SECRET'),
-    'redirect' => env('ODNOKLASSNIKI_REDIRECT_URI')
+		'client_id' => env('ODNOKLASSNIKI_CLIENT_ID'),
+		'client_public' => env('ODNOKLASSNIKI_CLIENT_PUBLIC'),
+		'client_secret' => env('ODNOKLASSNIKI_CLIENT_SECRET'),
+		'redirect' => env('ODNOKLASSNIKI_REDIRECT_URI')
     ],
     'telegram' => [
 		'bot' => env('TELEGRAM_BOT_NAME'),  // The bot's username
 		'client_id' => null,
 		'client_secret' => env('TELEGRAM_TOKEN'),
 		'redirect' => env('TELEGRAM_REDIRECT_URI'),
-    ]
+	],
+	'yandex_metrica_counter' => env('YANDEX_METRICA_COUNTER'),
 ];
--- resources/views/livewire/includes/yandex-metrika.blade.php
+++ resources/views/livewire/includes/yandex-metrika.blade.php
@@ -6,12 +6,12 @@
    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(99378080, "init", {
+   ym({{ config('services.yandex_metrica_counter') }}, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
    });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/99378080" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/{{ config('services.yandex_metrica_counter') }}" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
